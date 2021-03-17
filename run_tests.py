99766921c2b7768c6121c04988dc23467eb37a92
pooja maheshwari <pooja.chugh15@gmail.com>
2021-03-17 22:44:39 +0000

    first commit

---
+++ run_tests.py
@@ -0,0 +1,6 @@
+import unittest
+from tests.compare_test import TestCompare
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ src/compare.py
@@ -0,0 +1,9 @@
+     
+def compare(num_1, num_2):
+    if num_1 == num_2:
+        return "numbers are the same"
+    elif num_1 < num_2:
+        return f"{num_1} is less than {num_2}"
+    else:
+        return f"{num_1} is greater than {num_2}"
+
